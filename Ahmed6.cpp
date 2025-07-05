5fbec920b9c23512c81770057cfbc3a20198a83e
Matter <amatter619@gmail.com>
2025-07-05 13:55:31 +0300

    last one

---
+++ Ahmed6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number > 0)
+        cout << "The number is positive." << endl;
+    else if (number < 0)
+        cout << "The number is negative." << endl;
+    else
+        cout << "The number is zero." << endl;
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout << "Enter a positive number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum from 1 to " << n << " is: " << sum << endl;
+
+    return 0;
+}
